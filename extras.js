1984ba5d42efd7be2841a7aa3a5b05af7367a803
Peter Johnson <integral.sketch@gmail.com>
2022-06-14 04:10:21 +0000

    gitpod won't host

--- extras.js
+++ extras.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
-
 function diffLines(code) {
     // find lines until you see [lh! +] or [lh! -]
-    const lines = removeStartingNewlines(code).split('\n');
+    const lines = code.split('\n');
     console.log(lines);
     let positions = []; // [[], [], []...]
     for (let i = 0; i<lines.length; i++) {
@@ -13,21 +11,16 @@ function diffLines(code) {
             positions.push([i+1, '-']);
         }
     };
-    return positions;
+    let posObj = {
+        positions: [],
+        types: []
+    };
+
+    for (let i = 0; i<positions.length; i++) {
+        posObj.positions.push(positions[i][0]);
+        posObj.types.push(positions[i][1]);
+    };
+    return posObj;
 };
 
-function removeStartingNewlines(code) {
-    // split by newlines
-    const lines = code.split('\n');
-    while (lines[0] == '') {
-        lines.shift();
-    }
-    return lines.join('\n');
-};
-
-async function getThemeFile(themeName) {
-    let themeFile;
-    fs.readFile('./themes/' + themeName + '.json', (err, dat) => {
-        
-    }) 
-}
+module.exports = diffLines;
--- highlight.js
+++ highlight.js
@@ -1,6 +1,7 @@
 const shiki = require('shiki');
 const fs = require('fs');
-
+const diffLines = require('./extras.js');
+const HTMLParser = require('node-html-parser');
 
 /* 
 ...
@@ -13,6 +14,11 @@ properties: {
         }
 ...
 */
+function plusminus(char) {
+    return char == '+' ? true : false;
+}
+
+
 async function getHTML(options) {
     let Theme;
     let error;
@@ -34,15 +40,16 @@ async function getHTML(options) {
        */
     };
 
-    const lineNum = options.lineNumbers  || true;
+    const lineNum = options.lineNumbers || false;
     const lang = options.lang;
 
     const highlight = await shiki.getHighlighter({
         theme: Theme
     });
 
+    const difflines = diffLines(options.code);
+    console.log(difflines);
     /*
-    const difflines = diffLines(code);
     const highlights = highlights(code);
     */
 
@@ -61,6 +68,14 @@ async function getHTML(options) {
     let bgcolor = src.colors["editor.background"];
 
     console.log(`${options.theme}: ${bgcolor}`)
+    for (let i = 0; i<src.tokenColors.length; i++) {
+            let curr = src.tokenColors[i];
+            if (Array.isArray(curr.scope) && curr.scope[0] == 'markup.deleted') {
+                minusFG = curr.settings.foreground;
+                minusBG = curr.settings.background || src.colors["diffEditor.removedTextBackground"];
+            };
+    };
+    
 
     if (src.tokenColors[0].settings.background) {
         console.log(src.tokenColors[0].settings.background);
@@ -73,10 +88,28 @@ async function getHTML(options) {
 
     primitiveLines = primitiveLines.map((el, index) => {
         if (index == 0) {
-                let noPre = el.replace(`<pre class="shiki" style="background-color: ${bgcolor}">`, '').replace('<code>', '');
-                return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + (lineNum ? `<span class="line-number" style="color:${linecolors}; text-align: right; -webkit-user-select: none; user-select: none;">${index+1}</span>` : '') + `${noPre}`;
+                let noPre = el.replace(`<pre class="shiki" style="background-color: ${bgcolor}">`, '').replace('<code>', '').replace(`[lh! +]`, '').replace(`[lh! -]`, '');
+                if (difflines.positions.includes(1)) {
+                    return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + (lineNum ? `<span class="line-number" style="color:${minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[0]}</span>` : '') + `<span style="background-color:${minusBG}; color:${minusFG}">` + `${noPre}` + `</span>`;
+                } else {
+                    return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + (lineNum ? `<span class="line-number" style="color:${linecolors}; text-align: right; -webkit-user-select: none; user-select: none;">${index+1}</span>` : '') + `${noPre}`;
+                }
         } else {
-            return (lineNum ? `<span class="line-number" style="color:${linecolors}; text-align: right; -webkit-user-select: none; user-select: none;">${index+1}</span>` : '') + `${el}`;
+            if (difflines.positions.includes(index+1)) {
+                let str = (lineNum ? `<span class="line-number" style="color:${minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[difflines.positions.indexOf(index+1)]}</span>` : '');
+                let nEl = el.replace(`[lh! +]`, '').replace(`[lh! -]`, '');
+                let root = HTMLParser.parse(nEl);
+                console.log(root.childNodes[0]);
+                for (let i = 0; i<root.childNodes.length; i++) {
+                    for (let j = 0; j<root.childNodes[i].childNodes.length; j++) {
+                        root.childNodes[i].childNodes[j].rawAttrs = `style=color:${minusFG}`;
+                    }
+                };
+                nEl = root.toString();
+                console.log(nEl);
+                return `<div style="background-color:${minusBG} !important; color:${minusFG} !important; display: inline;" class="line"` + str + nEl + '</div>';
+            }
+            return `<div class="line">` + (lineNum ? `<span class="line-number" style="color:${linecolors}; text-align: right; -webkit-user-select: none; user-select: none;">${index+1}</span>` : '') + `${el}` + '</div>';
         }
     });
     
