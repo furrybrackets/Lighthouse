a8e4ceb5d4550b6e6d44a97ef324d86812a9fb46
Peter Johnson <integral.sketch@gmail.com>
2022-06-14 05:51:40 +0000

    ssas

--- highlight.js
+++ highlight.js
@@ -15,7 +15,7 @@ properties: {
 ...
 */
 function plusminus(char) {
-    return char == '+' ? true : false;
+    return char == '+';
 }
 
 
@@ -57,11 +57,13 @@ async function getHTML(options) {
 
     let minusFG;
     let minusBG;
+    let plusBG;
+    let plusFG;
 
     const src = await JSON.parse(fs.readFileSync(`./themes/${options.theme}.json`));
 
 
-    const linecolors = src.colors["editorLineNumber.foreground"];
+    const linecolors = src.colors["editorLineNumber.foreground"] || src.colors["editor.foreground"];
 
     console.log(linecolors);
 
@@ -70,10 +72,13 @@ async function getHTML(options) {
     console.log(`${options.theme}: ${bgcolor}`)
     for (let i = 0; i<src.tokenColors.length; i++) {
             let curr = src.tokenColors[i];
-            if (Array.isArray(curr.scope) && curr.scope[0] == 'markup.deleted') {
+            if ((Array.isArray(curr.scope) && curr.scope[0] == 'markup.deleted') || curr.scope == 'markup.deleted') {
                 minusFG = curr.settings.foreground;
                 minusBG = curr.settings.background || src.colors["diffEditor.removedTextBackground"];
-            };
+            } else if ((Array.isArray(curr.scope) && curr.scope[0] == 'markup.inserted') || curr.scope == 'markup.inserted') {
+                plusFG = curr.settings.foreground;
+                plusBG = curr.settings.background;
+            }
     };
     
 
@@ -91,40 +96,46 @@ async function getHTML(options) {
         if (index == 0) {
                 let noPre = el.replace(`<pre class="shiki" style="background-color: ${bgcolor}">`, '').replace('<code>', '').replace(`[lh! +]`, '').replace(`[lh! -]`, '');
                 if (difflines.positions.includes(1)) {
+                    const type = difflines.types[0];
                     let root = HTMLParser.parse(noPre);
                     for (let i = 0; i<root.childNodes.length; i++) {
                         for (let j = 0; j<root.childNodes[i].childNodes.length; j++) {
-                            root.childNodes[i].childNodes[j].rawAttrs = `style=color:${minusFG}`;
+                            root.childNodes[i].childNodes[j].rawAttrs = `style=color:${plusminus(type) ? plusFG : minusFG}`;
                         }
                     };
                     noPre = root.toString();
-                    return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + `<div style="background-color:${minusBG} !important; display: inline;" class="line">` + (lineNum ? `<span class="line-number" style="color:${minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[0]}</span>` : '') + `${noPre}` + `</span>` + `</div>`;
+        
+                    return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + `<div style="background-color:${plusminus(type) ? plusBG : minusBG} !important; display: inline;" class="line">` + (lineNum ? `<span class="line-number" style="color:${plusminus(type) ? plusFG : minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[0]}</span>` : '') + `${noPre}` + `</span>` + `</div>`;
                 } else {
                     return `<pre class="shiki" style="background-color: ${bgcolor}"><code>` + `<div class="line">` + (lineNum ? `<span class="line-number" style="color:${linecolors}; text-align: right; -webkit-user-select: none; user-select: none;">${index+1}</span>` : '') + `${noPre}` + '</div>';
                 }
         } else {
             if (difflines.positions.includes(index+1)) {
-                let str = (lineNum ? `<span class="line-number" style="color:${minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[difflines.positions.indexOf(index+1)]}</span>` : '');
+                const type = difflines.types[0];
+                difflines.types[difflines.positions.indexOf(index+1)];
+                let str = (lineNum ? `<span class="line-number" style="color:${plusminus(type) ? plusFG : minusFG}; text-align: right; -webkit-user-select: none; user-select: none;">${difflines.types[difflines.positions.indexOf(index+1)]}</span>` : '');
                 let nEl = el.replace(`[lh! +]`, '').replace(`[lh! -]`, '');
                 let root = HTMLParser.parse(nEl);
                 for (let i = 0; i<root.childNodes.length; i++) {
                     for (let j = 0; j<root.childNodes[i].childNodes.length; j++) {
-                        root.childNodes[i].childNodes[j].rawAttrs = `style=color:${minusFG}`;
+                        console.log(type);
+                        root.childNodes[i].childNodes[j].rawAttrs = `style=color:${plusminus(type) ? plusFG : minusFG}`;
                     }
                 };
                 nEl = root.toString();
-                return `<div style="background-color:${minusBG} !important; color:${minusFG} !important; display: inline;" class="line"` + str + nEl + '</div>';
+                return `<div style="background-color:${plusminus(type) ? plusBG : minusBG} !important; color:${plusminus(type) ? plusFG : minusFG} !important; display: inline;" class="line"` + str + nEl + '</div>';
             }
             if (el.includes(`</code>`)) {
                 // remove the </code></pre>
-                let noEnd = el.replace('</code></pre>');
+                let noEnd = el.replace('</code></pre>')
                 
                 if (difflines.positions.includes(index+1)) {
+                    const type = difflines.type[0];
                     noEnd = el.replace(`[lh! +]`, '').replace(`[lh! -]`, '');
                     let root = HTMLParser.parse(noEnd);
                     for (let i = 0; i<root.childNodes.length; i++) {
                         for (let j = 0; j<root.childNodes[i].childNodes.length; j++) {
-                            root.childNodes[i].childNodes[j].rawAttrs = `style=color:${minusFG}`;
+                            root.childNodes[i].childNodes[j].rawAttrs = `style=color:${plusminus(type) ? plusFG : minusFG}`;
                         }
                     };
                     noEnd = root.toString();
